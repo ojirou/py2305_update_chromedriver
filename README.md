e82259341df1e4bd0b34bc0fcecd17364801e724
ojirou <myinaga@gmail.com>
2023-05-24 21:44:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"#py2305_update_chromedriver" 
---
+++ update_chromedriver.py
@@ -0,0 +1,18 @@
+import os
+import datetime
+import shutil
+ZipPath=r'C:\\Users\\user\\Downloads\\chromedriver_win32.zip'
+FilePath=r'C:\\Users\\user\\Downloads\\chromedriver.exe'
+Dir=r'C:\\Users\\user\\Downloads'
+DstDir1=r'C:\\Users\\user\\git\\py\\driver'
+DstDir2=r'C:\\Users\\user\\git\\web_scraping\\driver'
+DstDir3=r'C:\\Users\\user\\python\\web_scraping\\driver'
+DstDirs=[DstDir1,DstDir2,DstDir3]
+shutil.unpack_archive(ZipPath, Dir)
+today=datetime.date.today()
+num=1
+for DstDir in DstDirs:
+    BkDir=DstDir+'\\version_used_until_{:%y%m%d}'.format(today,num)
+    os.mkdir(BkDir)
+    shutil.move(DstDir+'\\chromedriver.exe', BkDir)
+    shutil.copy(FilePath, DstDir)
